d9b58a2d5b6667e1b89607d6d0c3678d6f51f48d
rodrigoaalmeida <rodrigo_a.almeida@outlook.com>
2020-06-07 18:04:07 -0400

    Dados incluidos pelo javascript

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "QTH0HeVxhtU",
+        title: "COD Warzone detonando um helicóptero",
+        duration: "11 seg",
+        price: "Free"
+    },
+    {
+        id: "OxezL1gCv18",
+        title: "COD Warzone 2",
+        duration: "15 seg",
+        price: "Free"
+    },
+    {
+        id: "-x-3Ej0kSIk",
+        title: "COD Warzone 3",
+        duration: "10 seg",
+        price: "Free"
+    },
+    {
+        id: "hn-WUgEGgIE",
+        title: "COD Warzone 5",
+        duration: "10 seg",
+        price: "Free"
+    },
+    {
+        id: "PWftVerflKA",
+        title: "COD Warzone 6",
+        duration: "10 seg",
+        price: "Free"
+    },
+    {
+        id: "JmQbomPYjfc",
+        title: "COD Warzone 8",
+        duration: "10 seg",
+        price: "Free"
+    }
+]
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 
+const videos = require("./data")
 const server = express()
 
 server.use(express.static('public'))
@@ -8,16 +9,30 @@ server.use(express.static('public'))
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 //=== rotas ===
 server.get("/", function(req, res){
-    return res.render("sobre")
+    const sobre = {
+        avatar_url: "https://avatars1.githubusercontent.com/u/42939426?s=460&u=6a2a721b6e94d314acebc0ceedd5113824010b10&v=4",
+        name: "Rodrigo Almeida",
+        role: "Programador júnior - Freelancer",
+        description: `<a href="https://github.com/rodrigoaalmeida" target="_blank">Programador</a> em aprendizado contínuo,
+         tenho a meta de trabalhar na área antes de chegar aos trinta e cinco anos, e pretendo me aposentar por ela algum
+          dia, para isso estou aprendendo web e inglês.`,
+        links: [
+            {name: "Github", url: "https://github.com/rodrigoaalmeida"},
+            {name: "Linkedin", url: "https://www.linkedin.com/in/rodrigo-andrade-de-almeida-a65b97167/"}
+        ]
+    }
+
+    return res.render("sobre", {sobre})
 })
 
 server.get("/portfolio", function(req, res){
-    return res.render("portfolio")
+    return res.render("portfolio", { items: videos})
 })
 
 //informa a porta que o servidor fica ouvindo
